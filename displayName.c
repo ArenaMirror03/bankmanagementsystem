3b457e53c9b1a0f20b6d7594a5953417f06d8a88
tarique2k13 <tarique2k13@gmail.com>
2024-11-13 17:54:44 +0530

    this is a odd even

--- displayName.c
+++ displayName.c
@@ -1,11 +1,28 @@
 #include<stdio.h>
 
+char number(int x)
+{
+if(x%2==0)
+{
+    printf("\n Even number :");
+}
+else
+{
+    printf("\n odd number");
+
+}
+
+
+}
 
 void tarique_display();
 int main()
 {
  tarique_display();
-
+ int a;
+printf("\n Please enter any number ");
+scanf("%d",&a);
+printf("%c",number(a));
 
     return 0;
 
