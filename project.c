ef6b0f005234f4b6e158233ead7942ba95725e70
Kaushikmishra10 <kaushikmsra10@gmail.com>
2024-11-13 17:40:20 +0530

    program to check even or odd

--- project.c
+++ project.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 
 void kaushik_display();
+void check_even_odd();
+int number;
 
 int main()
 {
 
    kaushik_display();
+   printf("\nenter any number : ");
+   scanf("%d",&number);
+
+   check_even_odd(); 
  
     return 0;
 }
@@ -13,4 +19,17 @@ int main()
 void kaushik_display()
 {
     printf("This is kaushik");
+}
+
+void check_even_odd()
+{
+    if(number % 2 == 0)
+    {
+        printf("this is even number");
+    }
+    else
+    {
+        printf("this is odd number");
+    }
+
 }
