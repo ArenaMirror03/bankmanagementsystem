c4fcccc4ccb19c69a9acf82fe7ced9cfb2e93ca7
ArenaMirror03 <officialsumitsharma03@gmail.com>
2024-11-14 18:08:31 +0530

    The sum of random 10 digit number

--- sumit_code/sum.c
+++ sumit_code/sum.c
@@ -1,34 +1,24 @@
-#include<stdio.h>
-int main()
-{
-    int a,b,c,d,e,f,g,h,i,j;
-     
-printf(" enter any number a: \n");
-scanf("%d", &a);
-printf(" enter any number b: \n");
-scanf("%d", &b);
-printf(" enter any number c: \n");
-scanf("%d", &c);
-printf(" enter any number d: \n");
-scanf("%d", &d);
-printf(" enter any number e: \n");
-scanf("%d", &e);
-printf(" enter any number f: \n");
-scanf("%d", &f);
-printf(" enter any number g: \n");
-scanf("%d", &g);
-printf(" enter any number h: \n");
-scanf("%d", &h);
-printf(" enter any number i: \n");
-scanf("%d", &i);
-printf(" enter any number j: \n");
-scanf("%d", &j);
+#include <stdio.h>
 
+int main() {
+    int numbers[10]; 
+    int sum = 0;      
+    
+    printf("Enter 10 numbers:\n");
 
-int sum= a+b+c+d+e+f+g+h+i+j;
+    
+    for (int i = 0; i < 10; i++) {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+    }
 
-printf("the sum of 10 number is %d", sum);
+    
+    for (int i = 0; i < 10; i++) {
+        sum += numbers[i];
+    }
 
-return 0;
+    
+    printf("The sum of the 10 numbers is: %d\n", sum);
 
+    return 0;
 }
