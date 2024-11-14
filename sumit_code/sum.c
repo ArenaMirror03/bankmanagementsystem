24d292136f33b8b179df37c0907e2a05f8c90b91
ArenaMirror03 <officialsumitsharma03@gmail.com>
2024-11-14 18:00:00 +0530

    The sum of 10 number

---
+++ sumit_code/sum.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f,g,h,i,j;
+     
+printf(" enter any number a: \n");
+scanf("%d", &a);
+printf(" enter any number b: \n");
+scanf("%d", &b);
+printf(" enter any number c: \n");
+scanf("%d", &c);
+printf(" enter any number d: \n");
+scanf("%d", &d);
+printf(" enter any number e: \n");
+scanf("%d", &e);
+printf(" enter any number f: \n");
+scanf("%d", &f);
+printf(" enter any number g: \n");
+scanf("%d", &g);
+printf(" enter any number h: \n");
+scanf("%d", &h);
+printf(" enter any number i: \n");
+scanf("%d", &i);
+printf(" enter any number j: \n");
+scanf("%d", &j);
+
+
+int sum= a+b+c+d+e+f+g+h+i+j;
+
+printf("the sum of 10 number is %d", sum);
+
+return 0;
+
+}
